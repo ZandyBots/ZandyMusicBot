3bde256a22aaaf81751ed904784577cdfd61a717
Zandy Bots <97863742+ZandyBots@users.noreply.github.com>
2022-01-17 20:46:06 +0530

    Add files via upload

---
+++ plugins/tr.py
@@ -0,0 +1,9 @@
+import os
+
+HELP_TEXT =f"**SETTINGS** \n\n`Click on the buttons below to get documentation about specific modules..`"
+START_TEXT = '👋 `Hey My Name Is` **Zandy Music Bot** \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
+STREAM_TEXT = '**COMMANDS** \n\n`/stream` - Reply to Video or File That You Want To stream In Vc. \n\n`/stream` - use [yt link] \n\n`/play` - reply to any audio file or utube url.. \n\n`/endstream` - Stop the stream'
+INFO_TEXT =f"**COMMANDS** \n\nGet information about something!\n\n`/info` - reply to any user!\n\n`/whois` - get the information of an user!"
+PASTE_TEXT = '**COMMANDS** \n\nPaste some texts or documents on pasting website!\n\n`/paste` - Reply To Any File!'
+VSONG_TEXT = '**COMMANDS** \n\nDo You Want Video Song!\n\n`/video` - **[Song Name]** Search Your Favourite Song!\n\n`/vsong` - **[Song Name]** Try!'
+TGPK_TEXT =f"**COMMANDS** \n\nDo as you wish with telegra.ph module!\n\n`/telegraph` - Reply to a supported media file!\n\n`/tgm` - Reply to a any msg or file."
